819988037738bac3b9fc6d61323d6255a5b48c19
jiahuanting <jht1995@126.com>
2020-01-29 23:52:42 +0800

    nCov

--- js/banner.js
+++
@@ -1,7 +0,0 @@
-$('.index-trigger')
-.on('mouseover', function() {
-    $(this).children('.index-dropdown').addClass('active')
-})
-.on('mouseout', function() {
-    $(this).children('.index-dropdown').removeClass('active')
-})
